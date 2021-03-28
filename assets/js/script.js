d430cc38385c0a2193cba16bb5006a98772e0ff8
Zain Haroon <zainharoon890@gmail.com>
2021-03-28 17:44:35 +0500

    Finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,78 @@
 $(function () {
-  $("#createProduct").click(createProduct);
+  $("#CreateProduct").click(createProduct);
+  $("#CanselCreate").click(CanselCreate);
   loadProducts();
 });
 
-function editProduct() {
-  console.log("INSIDE EDIT");
+function CanselCreate() {
+  alert("Cancelled Successfully");
+  $("#Createname").val("");
+  $("#Createprice").val("");
+  $("#Createcolor").val("");
+  $("#Createdepartment").val("");
+  $("#Createdescription").val("");
+  $("#collapseCreate").collapse("toggle");
 }
+// function editProduct() {
+//   console.log("INSIDE EDIT");
+// }
 function deleteProduct(e) {
   console.log("INSIDE DEL", e);
   $.ajax({
     url: "https://usman-recipes.herokuapp.com/api/products/" + e,
     method: "DELETE",
+    error: function (response) {
+      if (response.log == undefined) {
+        alert("INTERNET DOWN. Please Check your Internet Connection");
+      } else {
+        alert(response.log);
+      }
+    },
     success: function (response) {
       console.log(response);
+      alert("Delete was Successful");
       loadProducts();
     },
   });
 }
 function createProduct() {
   console.log("INSIDE Create");
+  const name = $("#Createname").val();
+  const price = $("#Createprice").val();
+  const color = $("#Createcolor").val();
+  const department = $("#Createdepartment").val();
+  const description = $("#Createdescription").val();
+
+  if (name === "" || price === "" || department === "" || description === "") {
+    alert("Please Do not leave any field Empty");
+  } else {
+    console.log(name, price, color, department, description);
+    $.ajax({
+      url: "https://usman-recipes.herokuapp.com/api/products",
+      method: "POST",
+      data: { name, price, color, department, description },
+      error: function (response) {
+        if (response.log == undefined) {
+          alert("INTERNET DOWN. Please Check your Internet Connection");
+        } else {
+          alert(response.log);
+        }
+      },
+      success: function (response) {
+        $("#Createname").val("");
+        $("#Createprice").val("");
+        $("#Createcolor").val("");
+        $("#Createdepartment").val("");
+        $("#Createdescription").val("");
+        $("#collapseCreate").collapse("toggle");
+        alert("Sending POST req. Please Check page for your product!");
+        loadProducts();
+      },
+    });
+  }
+  // $("#toggle_collapse").addClass(" collapsed");
+  // $("#toggle_collapse").attr("aria-expanded", "false");
+  // $("#collapseCreate").removeClass("show");
 }
 
 function loadProducts() {
@@ -26,12 +80,17 @@ function loadProducts() {
   $.ajax({
     url: "https://usman-recipes.herokuapp.com/api/products",
     method: "GET",
+    error: function (response) {
+      if (response.log == undefined) {
+        alert("INTERNET DOWN. Please Check your Internet Connection");
+      }
+    },
     success: function (response) {
       let maind = $("#mainDiv");
       for (let i = 0; i < response.length; i++) {
         let products = document.createElement("div");
         let delbtn = document.createElement("button");
-        let Editbtn = document.createElement("button");
+        // let Editbtn = document.createElement("button");
         let para = document.createElement("p");
         para.innerHTML = `<p>Price: $${response[i].price}</br>Department: <em>${response[i].department}</em></br>Description: ${response[i].description}</p>`;
 
@@ -40,10 +99,10 @@ function loadProducts() {
         delbtn.innerHTML = "Delete";
         // delbtn.onclick = () => deleteProduct(response[i]._id);
         delbtn.onclick = () => deleteProduct($(products).attr("id"));
-        Editbtn.className = "btn btn-warning";
-        Editbtn.id = "editBtn";
-        Editbtn.innerHTML = "Edit";
-        Editbtn.onclick = editProduct;
+        // Editbtn.className = "btn btn-warning";
+        // Editbtn.id = "editBtn";
+        // Editbtn.innerHTML = "Edit";
+        // Editbtn.onclick = editProduct;
 
         products.id = response[i]._id;
         // products.attributes = ("_id", response[i]._id);
@@ -53,7 +112,7 @@ function loadProducts() {
 
         products.innerHTML = `<h3>${response[i].name}</h3>`;
         products.append(delbtn);
-        products.appendChild(Editbtn);
+        // products.appendChild(Editbtn);
         products.append(para);
         maind.append(products);
       }
